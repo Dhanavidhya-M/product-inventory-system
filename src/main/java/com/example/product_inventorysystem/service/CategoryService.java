0901da6a9b857b8fe0310b04c29a432393accb9e
Dhanavidhya Maheswaran <dhanavidhya.maheswaran@kumaran.com>
2025-06-16 15:16:05 +0530

    pushing product-inventory-system

---
+++ src/main/java/com/example/product_inventorysystem/service/CategoryService.java
@@ -0,0 +1,44 @@
+package com.example.product_inventorysystem.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.example.product_inventorysystem.model.Category;
+import com.example.product_inventorysystem.repository.CategoryRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class CategoryService {
+
+	 CategoryRepository categoryRepository ;
+	
+	public Category getCategory(Long id) {
+		return categoryRepository.findById(id).orElse(null);
+	}
+	
+	public Category saveCategory(Category category) {
+		return categoryRepository.save(category);
+	}
+	
+	public List<Category> getAllCategories(){
+		return categoryRepository.findAll();
+	}
+
+	public void deleteCategory(Long id) {
+		categoryRepository.deleteById(id);
+	}
+
+	public boolean checkCatgeoryExist(String name) {
+		List<Category> categorylist=categoryRepository.findAll();
+		for(Category c:categorylist) {
+			if(c.getName().equalsIgnoreCase(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+
+}
---
+++ src/main/java/com/example/product_inventorysystem/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.example.product_inventorysystem.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.product_inventorysystem.model.Product;
+import com.example.product_inventorysystem.repository.ProductRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ProductService {
+
+	 ProductRepository productRepository;
+	
+	public Product getProduct(Long id) {
+		return productRepository.findById(id).orElse(null);
+	}
+	
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
+	
+	public List<Product> getAllProducts(){
+		return productRepository.findAll();
+	}
+
+	public void deleteProduct(Long id) {
+		productRepository.deleteById(id);
+	}
+	
+	
+}
